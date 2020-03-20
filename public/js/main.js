9ecf1072cc800aff3bd564807b80450f1b20a19e
Anh Nguyen <nghoanganh994@gmail.com>
2020-03-21 00:05:51 +0700

    Remove unused js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,27 @@
+
 $(document).ready(function () {
+    // var data = {};
+    //
+    // data.open = false;
+    // data.menu = $(".w-nav-menu");
+    // data.links = $(".menu-button.w-nav-button");
+    //
+    // var overlay = '<div class="w-nav-overlay" data-wf-ignore />';
+    //
+    // $(".w-icon-nav-menu").click( function () {
+    //
+    //     if (!data.open) {
+    //         //transform: translateY(0px) translateX(0px); transition: transform 400ms ease 0s;
+    //         data.menu.css({"transition": "transform 400ms ease 0s"});
+    //     }
+    //     data.menu.toggleClass("w--nav-menu-open");
+    //     data.links.toggleClass("w--open");
+    //
+    //     if (!data.open) {
+    //         data.menu.css({"transform": "translateY(100px)"});
+    //     }
+    // });
+
     $("#btn-post-comment").click(function () {
         var commentForm = {
             postId: $("#commentPostId").val(),
